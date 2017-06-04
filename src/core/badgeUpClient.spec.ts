300e5f4da2d93d4ca9289fc50d06b18c04753c9d
Erti-Chris Eelmaa <chriseelmaa@gmail.com>
2017-06-04 18:01:40 +0000

    added karma unit tests (#1)

---
+++ src/core/badgeUpClient.spec.ts
@@ -0,0 +1,65 @@
+import { BadgeUpClient } from './badgeUpClient';
+import { BadgeUpSettings } from '../config';
+import { BadgeUpStorage, BadgeUpEarnedAchievement, BadgeUpEvent, BadgeUpStoredEvent, BadgeUpNotificationType } from '../declarations';
+import { BadgeUpLogger } from './badgeUpLogger';
+import { BadgeUpToast } from './badgeUpToast';
+import { BadgeUpLocalStorage } from './badgeUpLocalStorage';
+
+describe('BadgeUpClient', () => {
+    let achievementId = 10;
+    let mockToast: BadgeUpToast = {
+        showNewAchievementEarned: (badgeUpEarnedAchievement: BadgeUpEarnedAchievement) => { }
+    };
+
+    let mockLogger: BadgeUpLogger = {
+        warn: message => { },
+        error: message => { }
+    };
+
+    let mockSettings: BadgeUpSettings = { apiKey: 'xxxxxxx' };
+    let mockStorage: BadgeUpStorage = new BadgeUpLocalStorage();
+
+    let mockBrowserClient = {
+        achievements: {
+            get: (achievementId: number) => {
+                return Promise.resolve({
+                    achievementId: achievementId,
+                    name: 'test-achievement',
+                    description: 'test achievement used for testing'
+                });
+            }
+        },
+
+        events: {
+            create: (event: BadgeUpEvent) => {
+                return Promise.resolve({
+                    progress: [{
+                        isComplete: true,
+                        achievementId: achievementId
+                    }]
+                });
+            }
+        }
+    };
+
+    it('#should receive notification when a new achievement is earned', (done) => {
+        let badgeUpClient = new BadgeUpClient(
+            mockLogger,
+            mockToast,
+            mockSettings,
+            mockStorage,
+            mockBrowserClient);
+
+        badgeUpClient.subscribe((notificationType: BadgeUpNotificationType, data: any) => {
+            if (notificationType === BadgeUpNotificationType.NewAchievementEarned) {
+                let earnedAchievement: BadgeUpEarnedAchievement = <BadgeUpEarnedAchievement>data;
+                expect(earnedAchievement.achievementId).toBe(achievementId);
+                done();
+            }
+        });
+
+        badgeUpClient.emit({
+            key: 'badgeup:test'
+        });
+    });
+});
