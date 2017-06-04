c48ba8a5fcd4b60b21c0d6512bd4e7724e611c60
chriseelmaa <chriseelmaa@gmail.com>
2017-06-04 12:07:31 +0100

    added karma unit tests

---
+++ karma.conf.js
@@ -0,0 +1,57 @@
+var webpackConfig = require('./webpack.test.js');
+
+module.exports = function (config) {
+  var _config = {
+    basePath: '',
+
+    frameworks: ['jasmine'],
+
+    files: [{
+      pattern: './karma-test-shim.js',
+      watched: true
+    }],
+
+    preprocessors: {
+      './karma-test-shim.js': ['webpack', 'sourcemap']
+    },
+
+    webpack: webpackConfig,
+
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
+
+    webpackServer: {
+      noInfo: true
+    },
+
+    browserConsoleLogOptions: {
+      level: 'log',
+      format: '%b %T: %m',
+      terminal: true
+    },
+
+    reporters: ['kjhtml', 'dots'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+
+    browsers: ['ChromeNoSandboxHeadless', 'Chrome'],
+
+    customLaunchers: {
+      ChromeNoSandboxHeadless: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--disable-web-security',
+          '--no-sandbox',
+          '--disable-gpu'
+        ]
+      }
+    },
+
+    singleRun: false
+  };
+
+  config.set(_config);
+};
