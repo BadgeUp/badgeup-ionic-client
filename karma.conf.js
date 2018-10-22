2f0a0b6e9af51d2c20a9a836cfd42288a17fb51f
Mark Herhold <mark.herhold@gmail.com>
2018-10-22 04:15:01 +0000

    updating karma

--- karma.conf.js
+++ karma.conf.js
@@ -3,42 +3,24 @@ const webpackConfig = require('./webpack.test.js');
 module.exports = function (config) {
   const _config = {
     basePath: '',
-
     frameworks: ['jasmine'],
-
     files: [{
       pattern: './karma-test-shim.js',
       watched: true
     }],
-
     preprocessors: {
       './karma-test-shim.js': ['webpack', 'sourcemap']
     },
-
     webpack: webpackConfig,
-
     webpackMiddleware: {
       stats: 'errors-only'
     },
-
-    webpackServer: {
-      noInfo: true
-    },
-
-    browserConsoleLogOptions: {
-      level: 'log',
-      format: '%b %T: %m',
-      terminal: true
-    },
-
     reporters: ['kjhtml', 'dots'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-
     browsers: ['ChromeHeadless', 'Chrome'],
-
     singleRun: false
   };
 
