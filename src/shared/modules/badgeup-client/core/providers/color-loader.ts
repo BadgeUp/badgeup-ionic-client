a535156cf82309d33b8e5abb04bff9ed644e7932
Mark Herhold <mark.herhold@gmail.com>
2019-01-24 22:56:50 -0500

    adding comments

--- src/shared/modules/badgeup-client/core/providers/color-loader.ts
+++ src/shared/modules/badgeup-client/core/providers/color-loader.ts
@@ -8,7 +8,11 @@ import { Injectable } from '@angular/core';
 export class ColorLoaderProvider {
 
     public readonly BASE_RED_COLOR = '#F12049';
-    
+
+    /**
+     * Retrieves the primary color as a hex color string
+     * @param url achievement icon image URL
+     */
     public async getColor(url: string): Promise<string> {
         const res = await fetch(url, { method: 'HEAD' });
         return res.headers.get('x-goog-meta-primarycolor');
