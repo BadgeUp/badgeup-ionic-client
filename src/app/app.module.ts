28c2d8696334fa52c82ce69729654442d2be2d21
Mark Herhold <mark.herhold@gmail.com>
2019-01-29 14:06:08 +0000

    adding Sentry error reporting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,28 +9,46 @@ import { BadgeupClientDemoApp } from './app.component';
 import { AppComponentsModule } from '../components/app-components.module';
 import { EarnedAchievementsModalComponent } from '../components/earned-achievements-modal/earned-achievements-modal';
 import { AchievementsOverviewModalComponent } from '../components/achievements-overview-modal/achievements-overview-modal';
+import { init, captureException } from '@sentry/browser';
+
+/**
+ * Error reports sent to the BadgeUp team
+ * Do not use this outside of the demo codebase
+ */
+init({ dsn: 'https://856122adb81449ea9c92338df37423a1@sentry.io/1381819' });
+
+export class CustomErrorHandler extends IonicErrorHandler {
+    constructor() {
+        super();
+    }
+
+    public handleError(err: any): void {
+        super.handleError(err);
+        captureException(err);
+    }
+}
 
 @NgModule({
-  declarations: [
-    BadgeupClientDemoApp,
-    HomePage,
-  ],
-  imports: [
-    BrowserModule,
-    AppComponentsModule,
-    IonicModule.forRoot(BadgeupClientDemoApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    AchievementsOverviewModalComponent,
-    EarnedAchievementsModalComponent,
-    BadgeupClientDemoApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
-  ]
+    declarations: [
+        BadgeupClientDemoApp,
+        HomePage,
+    ],
+    imports: [
+        BrowserModule,
+        AppComponentsModule,
+        IonicModule.forRoot(BadgeupClientDemoApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        AchievementsOverviewModalComponent,
+        EarnedAchievementsModalComponent,
+        BadgeupClientDemoApp,
+        HomePage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: ErrorHandler, useClass: CustomErrorHandler }
+    ]
 })
 export class AppModule { }
